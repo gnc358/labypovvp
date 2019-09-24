10a5d0373ce3d10d4d1e6ddab4c357baa61683b1
gnc358 <55323744+gnc358@users.noreply.github.com>
2019-09-24 20:42:11 +0300

    Add files via upload

---
+++ Laba1.m
@@ -0,0 +1,16 @@
+a=-1.25;
+d=2.5;
+i=5;
+c=0.05;
+x=1.35;
+f=sqrt(abs(c-d)+power(a+c,2));
+h=sin(2*i);
+k=power(10,-3);
+l=exp(i*x);
+m=abs(c-d)+power(a,2);
+n=power((a+c)*(a+c),1/3);
+y=(f/h)+(k*l)-(m/n);
+
+p=[a d i c x y];
+o=find(p>0)
+p=[length(o) p]
